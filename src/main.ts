8eb5f8ff6f389c5a8cc5e7b969310176035bd671
Abhinav-Prakashh <abhinavprakash355@gmail.com>
2025-11-08 14:17:16 +0530

    updated tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { apiKey } from "./apiKey.js";
+// import { apiKey } from "./apiKey.js";
+const apiKey = process.env.API_KEY as string;
 
 const $cityInput = $("#cityInput");
 const $getWeatherBtn = $("#getWeatherBtn");
