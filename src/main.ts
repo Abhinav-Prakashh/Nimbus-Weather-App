54d8be32dea7c8b8ea0c614d603fbc2457dc3c0b
Abhinav-Prakashh <abhinavprakash355@gmail.com>
2025-11-08 12:15:51 +0530

    added images, added more typescript and html
    Added image support , and added sakura petals for better ui look

--- src/main.ts
+++ src/main.ts
@@ -28,16 +28,30 @@ $("#getWeatherBtn").on("click", async () => {
         return;
     }
 
+    const $weatherImg = $("#weather-img");
+        function getWeatherImage(description: string): string {
+            const desc = description.toLowerCase();
+
+            if (desc.includes("cloud")) return "assets/PartlyCloudy.png";
+            if (desc.includes("rain")) return "assets/rainy.png";
+            if (desc.includes("thunder")) return "assets/ThunderStorm.png";
+            if(desc.includes("clear") || desc.includes("sunny")) return "assets/Sunny.png";
+            return "assets/default.png";
+        }
+
     try {
         const data = await fetchWeather(city);
 
         $weatherCity.text(data.name);
         $weatherTemp.text(`${data.main.temp}°C`);
         $weatherDesc.text(data.weather[0].description);
+        $weatherImg.attr("src", getWeatherImage(data.weather[0].description));
 
         // Show weather screen
         $mainContainer.addClass("hidden");
         $weatherScreen.removeClass("hidden");
+        //add sakura petals
+        createSakuraPetals();
 
     } catch (err) {
         alert("City not found!");
@@ -47,4 +61,23 @@ $("#getWeatherBtn").on("click", async () => {
 $backBtn.on("click", () => {
     $weatherScreen.addClass("hidden");
     $mainContainer.removeClass("hidden");
+    $(".sakura-container").empty();
 });
+
+function createSakuraPetals() {
+    const $container = $(".sakura-container");
+    const numPetals = 20;
+
+    $container.empty();
+    for(let i = 0; i < numPetals; i++) {
+        const $petal = $("<div></div>").addClass("sakura");
+        $petal.css({
+        left: `${Math.random() * 100}vw`,
+        animationDelay: `${Math.random() * 10}s`,
+        animationDuration: `${8 + Math.random() * 5}s`,
+        width: `${10 + Math.random() * 10}px`,
+        height: `${10 + Math.random() * 10}px`
+        });
+        $container.append($petal);
+    }
+}
